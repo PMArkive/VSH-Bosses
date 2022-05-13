2ada0cd1491241329e0b51d979447fec81642a00
Aidan Sanders <aidan.sanders1316@gmail.com>
2022-05-13 17:53:18 +0930

    saxtonhale include

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,7 @@ cp -r ../addons/sourcemod/scripting addons/sourcemod
 cd addons/sourcemod/scripting
 
 # Install Dependencies
-wget "https://raw.githubusercontent.com/redsunservers/VSH-Rewrite/master/addons/sourcemod/scripting/include/saxtonhale.inc" --O include/saxtonhale.inc
+wget "https://raw.githubusercontent.com/redsunservers/VSH-Rewrite/master/addons/sourcemod/scripting/include/saxtonhale.inc" -O include/saxtonhale.inc
 wget "https://raw.githubusercontent.com/peace-maker/DHooks2/dynhooks/sourcemod_files/scripting/include/dhooks.inc" -O include/dhooks.inc
 wget "https://raw.githubusercontent.com/nosoop/SM-TFEconData/master/scripting/include/tf_econ_data.inc" -O include/tf_econ_data.inc
 wget "https://raw.githubusercontent.com/FlaminSarge/tf2attributes/master/tf2attributes.inc" -O include/tf2attributes.inc
